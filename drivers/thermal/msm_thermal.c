0c1d72c2891abe47970a29aaefbde01d09083818
kasperhettinga <kasper.hettinga@gmail.com>
2014-02-13 15:28:25 +0100

    Add thermal limit sysfs
    Change-Id: Ic0ac6b9b6d6995e8f78a57172fabb0e816c148d8

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -41,6 +41,9 @@
 #define MAX_RAILS 5
 #define MAX_THRESHOLD 2
 
+unsigned int temp_threshold = 75;
+module_param(temp_threshold, int, 0644);
+
 static struct msm_thermal_data msm_thermal_info;
 static struct delayed_work check_temp_work;
 static bool core_control_enabled;
@@ -1099,7 +1102,7 @@ static void __ref do_freq_control(long temp)
 	uint32_t cpu = 0;
 	uint32_t max_freq = cpus[cpu].limited_max_freq;
 
-	if (temp >= msm_thermal_info.limit_temp_degC) {
+	if (temp >= temp_threshold) {
 		if (limit_idx == limit_idx_low)
 			return;
 
@@ -1107,7 +1110,7 @@ static void __ref do_freq_control(long temp)
 		if (limit_idx < limit_idx_low)
 			limit_idx = limit_idx_low;
 		max_freq = table[limit_idx].frequency;
-	} else if (temp < msm_thermal_info.limit_temp_degC -
+	} else if (temp < temp_threshold -
 		 msm_thermal_info.temp_hysteresis_degC) {
 		if (limit_idx == limit_idx_high)
 			return;
@@ -2239,7 +2242,7 @@ static int probe_vdd_rstr(struct device_node *node,
 				goto read_node_fail;
 		}
 
-		rails[i].curr_level = -1;
+		rails[i].curr_level = 0;
 		rails[i].reg = NULL;
 		i++;
 	}
